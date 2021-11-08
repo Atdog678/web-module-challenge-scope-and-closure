8c706e5173e955606fab7e292cff24ff2c3b5876
zach <zbeebe461@gmail.com>
2021-11-08 15:16:50 -0600

    completed work

--- README.md
+++ README.md
@@ -61,6 +61,7 @@ You will be running your tests in one terminal and debugging in the other. As yo
 Edit the `ReadMe` file with your answers to the questions below.
 
 1. In your own words, define closure (1-2 sentences).
+Closures are created when a function is made.
 2. Study the following code, then answer the questions below.
 
 ```js
@@ -82,8 +83,11 @@ dansRoll();
 ```
 
 a. Where is closure used in this code? How can you tell?
+function personalDice(name{  - Because thats when the function was made
 b. Compare and contrast calling `dansRoll` the first and second time. What is always the same? What could change?
+The output is the same. Zoe could change.
 c. What is the lexical scope of `newRoll`? 
+personalDice
 
 
 ### Task 3 - Stretch Goals
--- index.js
+++ index.js
@@ -28,9 +28,9 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+  Counter1 uses a closure
   2. Which of the two uses a closure? How can you tell?
-  
+  Counter 1 because of the function counterMaker() line
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random() * 2);  
 }
 
 
@@ -81,9 +81,14 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningcb){
+  return {
+    Home: inningcb(),
+    Away: inningcb()
+  }
 }
+
+console.log(finalScore(inning));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
